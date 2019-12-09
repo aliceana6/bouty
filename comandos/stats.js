0c3c9fed583b8921e8550f38b4cdb6188cd8456f
Selphi <56371070+aliceana6@users.noreply.github.com>
2019-12-09 16:20:56 -0300

    Update stats.js

--- comandos/stats.js
+++ comandos/stats.js
@@ -1,20 +1,6 @@
 exports.run = (bot, message, args) => {
     const Discord = require('discord.js');
-
     if (message.author.id !== '470691663799648266') return message.channel.send('Somente meu dono pode utilizar este comando.');
-
-    /*let stats = args.join(" ");
-    if(!stats) return message.channel.send("Escreva algo antes.");
-    bot.user.setActivity(stats, {type: "WATCHING"});
-    bot.user.setPresence({
-        status: 'dnd',
-    })
-
-    let embed = new Discord.RichEmbed()
-    .setColor('#2EFE2E')
-    .setTitle('<:sucesso:493554594341847052> Status trocado!')
-    .addField('<:perturbar:493809490127486976> Novo status:', `${stats}`)
-    message.channel.send(embed)*/
     let embed = new Discord.RichEmbed()
     .setColor('#2EFE2E')
     .setTitle('<:sucesso:493554594341847052> Status trocado!')
