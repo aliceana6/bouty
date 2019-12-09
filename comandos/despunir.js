445db84e250075b59cfce2e7fece1d8b91f79db7
Selphi <56371070+aliceana6@users.noreply.github.com>
2019-12-09 13:37:20 -0300

    Update despunir.js

--- comandos/despunir.js
+++ comandos/despunir.js
@@ -1,12 +1,12 @@
 exports.run = (bot, message, args) => {
-    exports.run = (bot, message, args) => {
-        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('Você não tem permissão para utilizar este comando.')
-        const reason = args.slice(1).join(' ');
-        bot.unbanReason = reason;
-        bot.unbanAuth = message.author;
-        const user = args[0];
-        if (!user) return message.reply('Você deve fornecer um ID do usuário que deseja desbanir').catch(console.error);
-        message.guild.unban(user);
-        message.reply(`O usuário <@${user}> foi desbanido com sucesso.`)
-    };
+    //exports.run = (bot, message, args) => {
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('Você não tem permissão para utilizar este comando.')
+    const reason = args.slice(1).join(' ');
+    bot.unbanReason = reason;
+    bot.unbanAuth = message.author;
+    const user = args[0] ? args[0] : message.mentions.users.size > 0 ? message.mentions.users.first().id : false;
+    if (!user) return message.reply('Você deve fornecer um ID do usuário que deseja desbanir').catch(console.error);
+    message.guild.unban(user);
+    message.reply(`O usuário <@${user}> foi desbanido com sucesso.`)
+    //};
 }
