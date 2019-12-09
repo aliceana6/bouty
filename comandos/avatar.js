049ee0ebeb58773ea0e83585d44a97ddfbbd9f20
Selphi <56371070+aliceana6@users.noreply.github.com>
2019-12-09 13:35:08 -0300

    Update avatar.js

--- comandos/avatar.js
+++ comandos/avatar.js
@@ -1,17 +1,16 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(bot, message, args) => {
-    let member = message.mentions.users.first() || bot.users.get(args[0]) || message.author;
-  let avatar = member.displayAvatarURL;
-  if (avatar.endsWith(".gif")) {
-    avatar = `${member.displayAvatarURL}?size=2048`
-  }
-  let perfil = new Discord.RichEmbed()
-  .setTitle(`🖼 ${member.username}`)
-  .setColor(`#58FAF4`)
-  .setDescription(`**[Clique aqui para baixar a imagem](${avatar})**`)
-  .setImage(avatar)
-  message.channel.send(perfil)
+    let member = message.mentions.users.size > 0 ? message.mentions.users.first() : args[0] && bot.users.get(args[0]) ? bot.users.get(args[0]) : message.author;
+    let avatar = member.displayAvatarURL;
+    if (avatar.endsWith(".gif")) avatar = `${member.displayAvatarURL}?size=2048`
+    
+    let embed = new Discord.RichEmbed()
+    .setTitle(`🖼 ${member.username}`)
+    .setColor(`#58FAF4`)
+    .setDescription(`**[Clique aqui para baixar a imagem](${avatar})**`)
+    .setImage(avatar)
+    message.channel.send(embed)
 }
 
 module.exports.config = {
