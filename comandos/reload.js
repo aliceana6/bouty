b237a3c899a0132c0dae1289280d40fbb07341d0
Selphi <56371070+aliceana6@users.noreply.github.com>
2019-12-09 16:20:25 -0300

    Update reload.js

--- comandos/reload.js
+++ comandos/reload.js
@@ -1,15 +1,5 @@
 module.exports.run = async (bot, message, args) => {
     if (message.author.id !== '470691663799648266') return;
-
-    /*resetBot(message.channel)
-    async function resetBot(channel) {
-        let token = bot.token;
-        await message.react(':sucesso:493554594341847052')
-            .then(message => bot.destroy())
-            .then(message => bot.destroy())
-            .then(() => bot.login(token));
-        message.channel.send('`Bot reiniciado com sucesso.`')
-    }*/
     const resetBot = (message, client) => {
         let token = client.token;
         client.destroy()
